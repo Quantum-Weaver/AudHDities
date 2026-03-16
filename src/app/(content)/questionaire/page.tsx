069cb37a201aad0320a26e4dfc4c6a9e7caf2c02
Quantum-Weaver <audhdities@proton.me>
2026-03-15 20:27:49 -0500

    Create page.tsx

---
+++ src/app/(content)/questionaire/page.tsx
@@ -0,0 +1,12 @@
+import AcidTest from '@/components/guild/AcidTest';
+import { Page } from '@/components/layout/Page'
+
+export default function GatewayPage() {
+  return (
+    <Page>
+      <div className='section-emergency'>
+        <AcidTest />
+      </div>
+    </Page>
+  );
+}
