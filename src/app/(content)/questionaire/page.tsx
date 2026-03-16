5115e8ee4f4c145a41f14ab37641a6914c9d5376
Quantum-Weaver <audhdities@proton.me>
2026-03-16 12:29:18 -0500

    Update page.tsx

--- src/app/(content)/questionaire/page.tsx
+++ src/app/(content)/questionaire/page.tsx
@@ -4,9 +4,13 @@ import { Page } from '@/components/layout/Page'
 export default function GatewayPage() {
   return (
     <Page>
-      <div className='section-emergency'>
-        <AcidTest />
-      </div>
+      <main className="min-h-screen py-20 px-6">
+        <div className="container max-w-4xl mx-auto">
+          <div className='section-emergency'>
+            <AcidTest />
+          </div>
+        </div>
+      </main>
     </Page>
   );
 }
